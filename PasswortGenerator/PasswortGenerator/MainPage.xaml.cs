77d1ff1aad5a62ba1b9aa6f9ab982b752f236d41
jerome-1998 <jerome.eller9898@gmx.de>
2022-01-23 21:25:22 +0100

    Projektdateien hinzufügen.

---
+++ PasswortGenerator/PasswortGenerator/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace PasswortGenerator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        //Generate Password
+        private void btnGenerate_Clicked(object sender, EventArgs e)
+        {
+            //proof Checkboxes
+            bool haslc = cbLowerCase.IsChecked;
+            bool hashc = cbHigherCase.IsChecked;
+            bool hasd = cbDigits.IsChecked;
+            bool hassc = cbSpecChar.IsChecked;
+            int length;
+            //set Length
+            if(eLength.Text != "")
+            {
+                length = Convert.ToInt32(eLength.Text);
+            }
+            else
+            {
+                length = 16;
+            }
+            //Declare Password Class
+            Passwort pwd;
+            //If one Checkbox is True use this Constructor
+            if(haslc || hashc || hasd || hassc )
+            {
+                pwd = new Passwort(haslc, hashc, hasd, hassc, length);
+            }
+            else //else use Standardconstructor
+            {
+                pwd = new Passwort();
+            }
+            //Show Password
+            eGeneratedPW.Text = pwd.GeneratePassword();
+        }
+
+        private void btnCopyToClipboard_Clicked(object sender, EventArgs e)
+        {
+            Clipboard.SetTextAsync(eGeneratedPW.Text);
+        }
+    }
+}
---
+++ PasswortGenerator/PasswortGenerator/Passwort.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswortGenerator
+{
+    public class Passwort
+    {
+        //Declare Charsets
+        private int _length;
+        private string _charSetHC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private string _charSetLC = "abcdefghijklmnopqrstuvwxyz";
+        private string _charSetD = "1234567890";
+        private string _charSetSC = "!§$%&/()=?'*_-.:,;+#~^°<>|"+'"';
+        private string _charSet;
+
+        //Standardconstructor
+        public Passwort()
+        {
+            _length = 16;
+            _charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ12344567890!§$%&/()=?'*_-.:,;+#~^°<>|"+'"'+"abcdefghijklmnopqrstuvwxyz";
+        }
+        //Userspecific Constructor
+        public Passwort(bool hasLC, bool hasHC, bool hasD, bool hasSC, int length)
+        {
+            if (hasLC)
+            {
+                _charSet = _charSet + _charSetLC;
+            }
+            if (hasHC)
+            {
+                _charSet = _charSet + _charSetHC;
+            }
+            if (hasD)
+            {
+                _charSet = _charSet + _charSetD;
+            }
+            if (hasSC)
+            {
+                _charSet = _charSet + _charSetSC;
+            }
+
+            _length = length;
+        }
+        public int length
+        {
+            get
+            {
+                return _length;
+            }
+        }
+
+        public string charSet
+        {
+            get
+            {
+                return _charSet;
+            }
+        }
+
+        public string GeneratePassword()
+        {
+            string keyword = "";
+            //Generate Random Password
+            Random dice = new Random();
+            int dicefall;
+            for (int i = 0; i<length; i++)
+            {
+                dicefall = dice.Next(charSet.Length);
+                    
+                keyword = keyword + charSet[dicefall];
+            }
+            
+            return keyword;
+        }
+    }
+}
